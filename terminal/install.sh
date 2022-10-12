828419ce14406c86966f58cc5e753b0e2cfede0a
Simone Bembi <simone.bembi@gmail.com>
2022-10-12 10:49:31 +0200

    zsh installation

--- terminal/install.sh
+++ terminal/install.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/env bash
 set -e
 
+# Install ZSH
+sudo apt-get install -y zsh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
 echo 'alias cubbit="cd ~/cubbit"' >> ~/.bashrc
 echo 'alias cubbit-tilt="cubbit && tilt -f coordinator/Tiltfile"' >> ~/.bashrc
 echo 'alias cup="cubbit-tilt up"' >> ~/.bashrc
 echo 'alias cdown="cubbit-tilt down"' >> ~/.bashrc
 
+echo 'alias cubbit="cd ~/cubbit"' >> ~/.zshrc
+echo 'alias cubbit-tilt="cubbit && tilt -f coordinator/Tiltfile"' >> ~/.zshrc
+echo 'alias cup="cubbit-tilt up"' >> ~/.zshrc
+echo 'alias cdown="cubbit-tilt down"' >> ~/.zshrc
+
 source ~/.bashrc
+
+# Change default shell
+sudo chsh -s $(which zsh) coder
