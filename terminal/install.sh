f15f7a01579afe9604ebdbecbc403a1b1d5dfff3
Simone Bembi <simone.bembi@gmail.com>
2022-10-14 13:57:30 +0200

    aliases for web deps build

--- terminal/install.sh
+++ terminal/install.sh
@@ -19,6 +19,15 @@ echo 'alias cubbit-tilt="cubbit && tilt -f coordinator/Tiltfile"' >> ~/.zshrc
 echo 'alias cup="cubbit-tilt up"' >> ~/.zshrc
 echo 'alias cdown="cubbit-tilt down"' >> ~/.zshrc
 echo 'alias k="kubectl"' >> ~/.zshrc
+echo 'alias build-protos="cubbit && yarn workspace @cubbit/protos make"' >> ~/.zshrc
+echo 'alias build-ctypings="cubbit && yarn workspace @cubbit/ctypings make"' >> ~/.zshrc
+echo 'alias build-controllers="cubbit && yarn workspace @cubbit/controllers compile"' >> ~/.zshrc
+echo 'alias build-visual-element="cubbit && yarn workspace @cubbit/visual-element.new compile"' >> ~/.zshrc
+echo 'alias build-utils="cubbit && yarn workspace @cubbit/utils compile"' >> ~/.zshrc
+echo 'alias build-toggles="cubbit && yarn workspace @cubbit/toggles make"' >> ~/.zshrc
+echo 'alias build-core="cubbit && yarn workspace @cubbit/core compile"' >> ~/.zshrc
+echo 'alias build-redux-with-status="cubbit && yarn workspace @cubbit/redux-with-status compile"' >> ~/.zshrc
+echo 'alias build-web-deps="build-protos && build-ctypings && build-controllers && build-visual-element && build-utils && build-toggles && build-redux-with-status"' >> ~/.zshrc
 
 source ~/.bashrc
 
